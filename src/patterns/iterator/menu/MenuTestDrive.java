67bf37a4c636e71ec8e7153045d55dbe36b3cdf0
codetaro <codetaro@gmail.com>
2019-06-24 17:08:58 +1000

    p368 闪回到迭代器

--- src/patterns/iterator/menu/MenuTestDrive.java
+++ src/patterns/iterator/menu/MenuTestDrive.java
@@ -5,8 +5,9 @@ public class MenuTestDrive {
     public static void main(String[] args) {
         PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
         DinerMenu dinerMenu = new DinerMenu();
+        CafeMenu cafeMenu = new CafeMenu();
 
-        Waitress alice = new Waitress(pancakeHouseMenu, dinerMenu);
+        Waitress alice = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
 
         alice.printMenu();
     }
